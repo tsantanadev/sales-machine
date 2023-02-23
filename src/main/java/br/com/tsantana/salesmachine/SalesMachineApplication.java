2c10774f61bed8deed473910a414cc85bf4cefcb
Thiago Santana <thiago91098@gmail.com>
2023-02-22 21:25:36 -0300

    Project created

---
+++ src/main/java/br/com/tsantana/salesmachine/SalesMachineApplication.java
@@ -0,0 +1,13 @@
+package br.com.tsantana.salesmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tsantana/salesmachine/SalesMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tsantana.salesmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
